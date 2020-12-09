a6157444284994512d5385d40969bf05a3706d20
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-09 18:24:41 +0700

    create container

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled['div']`
+    width : 100%;
+    height : 100%;
+
+    position : fixed;
+
+    display : grid;
+    place-items : center;
+
+    background-color : ${ props => props['theme']['color']['primary'] };
+`
 
 const index = () => {
     return (
-        <div>
+        <Container>
             <p>Hello World</p>
-        </div>
+        </Container>
     )
 }
 
