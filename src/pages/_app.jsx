55dbcaba0293cb8005637bcf50df4297439ea2fa
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-09 17:58:10 +0700

    implement theme

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,9 +3,12 @@ import { ThemeProvider } from 'styled-components'
 
 import GlobalStyles from '../styles/global-styles'
 
+import LightTheme from '../theme/light-theme'
+import DarkTheme from '../theme/dark-theme'
+
 const MyApp = ({ Component, pageProps }) => {
     return (
-        <ThemeProvider>
+        <ThemeProvider theme={ LightTheme }>
             <GlobalStyles/>
             <Component {...pageProps} />
         </ThemeProvider>
