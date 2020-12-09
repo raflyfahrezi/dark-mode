5c1a174a6066f003db57ad56b5d7d46fada71282
Farhan Rafly Fahrezi <raflyfahr15@gmail.com>
2020-12-09 18:55:56 +0700

    implement redux

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
+import { Provider } from 'react-redux'
 
 import GlobalStyles from '../styles/global-styles'
 
 import LightTheme from '../theme/light-theme'
 import DarkTheme from '../theme/dark-theme'
 
+import Store from '../redux/store'
+
 const MyApp = ({ Component, pageProps }) => {
     return (
-        <ThemeProvider theme={ LightTheme }>
-            <GlobalStyles/>
-            <Component {...pageProps} />
-        </ThemeProvider>
+        <Provider store={ Store }>
+            <ThemeProvider theme={ LightTheme }>
+                <GlobalStyles/>
+                <Component {...pageProps} />
+            </ThemeProvider>
+        </Provider>
     )
 }
 
